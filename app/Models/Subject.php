22c0f15717ce317cf5ff490398c00ffda417a502
geraldon1997 <geraldon1997@gmail.com>
2023-02-06 07:29:39 +0100

    adjusted the migration file of grades

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,16 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToUser;
 use App\Traits\DefaultValueForCreatedByAndUpdatedBy;
 use App\Traits\MakeDateReadable;
-use App\Traits\TransformCreatedByAndUpdatedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subject extends Model
 {
-    use HasFactory, SoftDeletes, DefaultValueForCreatedByAndUpdatedBy, MakeDateReadable, TransformCreatedByAndUpdatedBy;
+    use HasFactory, SoftDeletes, DefaultValueForCreatedByAndUpdatedBy, MakeDateReadable, BelongsToUser;
 
     protected $fillable = ['name', 'active', 'deleted_at'];
 
